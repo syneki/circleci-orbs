9ea973af5bd4ba8a227523caadf3496e2a52b1d3
Martin PAUCOT <m.paucot@kmp.agency>
2022-09-14 17:47:31 +0200

    fix php-cs-fixer

--- php-cs-fixer/src/scripts/run.sh
+++ php-cs-fixer/src/scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-vendor/bin/php-cs-fixer fix --config="${PARAM_CONFIG}" -v --dry-run --stop-on-violation --using-cache=no "${PARAM_EXTRA_ARGS}"
+"$HOME/tools/php-cs-fixer/vendor/bin/php-cs-fixer" fix --config="${PARAM_CONFIG}" -v --dry-run --stop-on-violation --using-cache=no "${PARAM_EXTRA_ARGS}"
